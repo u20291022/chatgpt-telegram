68d9a9fe301b4b9aa845eae800f76254057c8138
u20291022 <u20291022@gmail.com>
2024-04-03 16:01:07 +0700

    fix

--- source/utils/chatgpt-history.ts
+++ source/utils/chatgpt-history.ts
@@ -17,6 +17,10 @@ class ChatGPTHistory {
   }
 
   public addMessage(userId: string | number, message: string, role: Role): void {
+    if (!this.history[userId]) {
+      this.history[userId] = [];
+    }
+    
     this.history[userId].push({ "role": role, "content": message });
 
     if (this.history[userId].length > 8) {
